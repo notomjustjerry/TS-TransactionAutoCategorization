4a7ba15852832831acf1f9a561e93894dc41f5d2
notomjustjerry <77520875+notomjustjerry@users.noreply.github.com>
2021-08-27 13:27:05 -0600

    Add files via upload

--- program.py
+++ program.py
@@ -1,42 +1,47 @@
 import csv
 import pandas as pd
 
-# SETUP
-# SORT CSV BY DEPOSIT (LOW TO HIGH) THEN TRANSACTION (A TO Z)
-# CTRL + SHIFT + Q FOR UPPERCASE
-# REMEMBER TO DELETE CREDITS
-category = "restaurants" # CHANGE CATEGORY LABEL
+category = "restaurantsdining" # UPDATE
+keywordList = []
 
-def labelCategory(x):
-    with open("./restaurants.txt") as file: # CHANGE TXT FILE
+def get_key_list():
+    global keywordList
+
+    with open("./KEYWORDLIST.txt") as file: # UPDATE
         keywordList = file.readlines()
-        for keyword in keywordList:
-            if keyword in x:
+    print(f"keywordList={keywordList}")
+    return keywordList
+
+def labelCategory(row):
+    if row.Credit > 0: # UPDATE
+        return None
+
+    label = str(row.Label_Category_Annotated) 
+    if label == 'nan':
+        label = ""
+
+    for keyword in keywordList:
+        if keyword in row.RawDescription:
+            if label == "":
                 return category
+            else:
+                return (label + "|" + category)
 
     return None
 
 def main():
-    # READ CSV, LABEL
-    df = pd.read_csv("./0a0a.csv")
-
-    # TREAT FILLED WITH SAME LABEL AS EMPTY
-    # df4 = df[df['Label_Category_Annotated']==category]
-    dfe = df[(df['Label_Category_Annotated'].isna()) | (df['Label_Category_Annotated']==category)]  # EMPTY or FILLED WITH SAME
-    dff = df[df['Label_Category_Annotated'].notna()] # FILLED and not FILLED WITH SAME & df['Label_Category_Annotated']!=category
-    print('dfe')
-    print(dfe['Label_Category_Annotated'].head(96))
-    print('dff')
-    print(dff['Label_Category_Annotated'].head(5))
     
+    keywordList = get_key_list()
+
+    # READ CSV, LABEL
+    df = pd.read_csv("./CSVNAMEHERE.csv") # UPDATE
 
     # CATEGORIZATION
-    dfe['Label_Category_Annotated'] = dfe['RawDescription'].apply( lambda x: labelCategory(x) )
-    dff['Comments'] = dff['RawDescription'].apply( lambda x: labelCategory(x) )
+    df['Label_Category_Annotated'] = df.apply( lambda row: labelCategory(row) , axis =1)
 
-    # PACKAGING
-    df3 = pd.concat([dfe, dff])
-    df3.to_csv('Zeroa0a.csv', index=False)
+    # SORTING AND PACKAGING
+    df.sort_values(['RawDescription', 'Credit', 'Debit'], ascending=[True, True, True], inplace=True)
+    df.to_csv('CSVNAMEHERE.csv', index=False) # UPDATE
 
 if __name__ == '__main__':
     main()
